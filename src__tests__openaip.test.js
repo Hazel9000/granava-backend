b5c468bc64f1ac882149357fb52fd55ce1ab495d
Hazel9000 <hazelchao2@gmail.com>
2025-05-21 01:08:19 -0700

    Delete src__tests__openaip.test.js

--- src__tests__openaip.test.js
+++
@@ -1,9 +0,0 @@
-const openAIPService = require('../services/openaip.service');
-
-describe('OpenAIP Service', () => {
-  test('should be defined', () => {
-    expect(openAIPService).toBeDefined();
-  });
-
-  // Add more tests as needed
-});
