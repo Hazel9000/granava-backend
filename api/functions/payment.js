ba5dcd9ea931ef6ef7f0e0b3e0a371efc237d7dc
Hazel9000 <hazelchao2@gmail.com>
2025-06-03 23:54:09 -0700

    Delete api/functions directory

--- api/functions/payment.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-  // In production: integrate with Stripe
-  // const stripe = require('stripe')(process.env.STRIPE_KEY);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ success: true })
-  };
-};
